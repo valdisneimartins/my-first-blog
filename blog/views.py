3d5f93cae5d050d7fd653d9a0d8b6bab8ca79870
Valdisnei <maldisneimartins@gmail.com>
2016-08-22 17:52:51 -0300

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -16,7 +16,6 @@ def post_detail(request,pk):
 	return render(request,'blog/post_detail.html', {'post':post})
 def post_new(request):
 	if request.method == "POST":
-	
 		form=PostForm(request.POST)
 		if form.is_valid():
 			post = form.save(commit=False)
